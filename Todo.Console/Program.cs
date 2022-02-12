048af4564419de0dd899f0e5a28d74f282441772
ilyass.lamrani <ilyass@outlook.com>
2022-02-12 15:26:13 +0100

    add console app

---
+++ Todo.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Microsoft.Extensions.DependencyInjection;
+using Todo.DependencyInjection;
+using Todo.Domain;
+using Todo.Service.Abstractions;
+
+var services = new ServiceCollection();
+
+services.AddTodoList();
+
+
+
+var provider = services.BuildServiceProvider();
+
+
+var service = provider.GetService<IService>();
+
+
+// dir li bghiti 
+service.AddTask(new Tasks()
+{
+    Details = "ssss"
+});
